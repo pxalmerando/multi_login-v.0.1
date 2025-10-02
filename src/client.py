3d85357dfe861b45f3ed707cd7b96c889ec1fee0
Angelou Lapad <ugwapings@gmail.com>
2025-10-02 12:43:41 +0800

    update src with documentation

--- src/client.py
+++ src/client.py
@@ -1,6 +1,19 @@
 from .token_manager import TokenManager
 class Client:
+    """
+        Client class used to interact with the API.
+
+        Args:
+            token_manager (TokenManager): Instance of TokenManager used to manage authentication tokens.
+    """
+    
     def __init__(self, token_manager: TokenManager):
         self.token_manager = token_manager
+
     def ensure_authenticated(self):
+        """
+            Ensures that the client has a valid access token before making requests to the API.
+
+            If the token has expired, it will be refreshed using the refresh token.
+        """
         self.token_manager.get_tokens()
--- src/exception_handler.py
+++ src/exception_handler.py
@@ -1,5 +1,10 @@
 from typing import Dict, Any, Optional
 class ApiError(Exception):
+    """
+        A base class for API errors.
+        This class contains properties for an error message, status code, response, and error code.
+    """
+
     error_type = "api_error"
     default_message = "An API error occurred"
 
@@ -20,6 +25,16 @@ class ApiError(Exception):
         return f"{self.__class__.__name__}(status_code={self.status_code}, message={self.message})"
 
     def _add_additional_fields(self, data: Dict[str, Any], fields: list[str]) -> Dict[str, Any]:
+        """
+            Add additional fields to the error dictionary.
+
+            Args:
+                data (Dict[str, Any]): The error dictionary.
+                fields (List[str]): The list of fields to add.
+
+            Returns:
+                Dict[str, Any]: The updated error dictionary.
+        """
         for field in fields:
             value = getattr(self, field, None)
             if value is not None:
@@ -36,14 +51,25 @@ class ApiError(Exception):
     
 
 class ClientError(ApiError):
+    """
+        A child class that overrides the error type for client errors.
+    """
     error_type = "client_error"
     
 class ServerError(ApiError):
+    """
+        A child class that overrides the error type for server errors.
+    """
     error_type = "server_error"
     
 
 class BadRequestError(ClientError):
-    default_message = "The request was invalid or malformed. Check the request parameters and try again. This type typically relates to wrong or invalid data being sent"
+    """
+        A child class that overrides the error type for bad request errors.
+        This type typically relates to wrong or invalid data being sent.
+    """
+
+    default_message = "The request was invalid or malformed. Check the request parameters and try again."
     status_code = 400
     def __init__(self, message: str = None, response: Optional[Any] = None, invalid_fields: Optional[list] = None):
 
@@ -56,14 +82,25 @@ class BadRequestError(ClientError):
         return self._add_additional_fields(data, ["invalid_fields"])
     
 class UnauthorizedError(ClientError):
+    """
+        A child class that overrides the error type for unauthorized errors.
+        This type typically relates to missing or invalid authentication credentials.
+    """
+
     default_message = "Authentication credentials are missing or invalid. Ensure you are using valid credentials and tokens"
     status_code = 401
     def __init__(self, message: str = None, response: Optional[Any] = None):
         super().__init__(message=message, response=response)
 
 class ForbiddenError(ClientError):
+    """
+        A child class that overrides the error type for forbidden errors.
+        This type typically relates to missing or invalid permissions.
+    """
+
     default_message = "The user does not have permission to access the requested resource."
     status_code = 403
+
     def __init__(self, message: str = None, response: Optional[Any] = None, required_permissions: Optional[str] = None):
         super().__init__(message=message, response=response)
 
@@ -74,31 +111,75 @@ class ForbiddenError(ClientError):
         return self._add_additional_fields(data, ["required_permissions"])
 
 class TooManyRequestsError(ClientError):
+    """
+        A child class that overrides the error type for too many requests errors.
+        This type typically relates to exceeding the rate limit.
+    """
+
     default_message = "Too many requests. You have exceeded the rate limit."
     status_code = 429
+
     def __init__(self, message: str = None, response: Optional[Any] = None, retry_after: Optional[int] = None, limit: Optional[int] = None):
-    
+        """
+            Initializes a TooManyRequestsError object.
+
+            Args:
+                message (str): The error message. Defaults to None.
+                response (Optional[Any]): The response from the server. Defaults to None.
+                retry_after (Optional[int]): The number of seconds to wait before retrying. Defaults to None.
+                limit (Optional[int]): The rate limit. Defaults to None.
+        """
         super().__init__(message=message, response=response)
         self.retry_after = retry_after
         self.limit = limit
 
     def to_dict(self):
+        """
+            Converts the error object to a dictionary.
+
+            Returns:
+                dict[str, Any]: The error object as a dictionary.
+        """
         data = super().to_dict()
         return self._add_additional_fields(data, ["retry_after", "limit"])
     
 class InternalServerError(ServerError):
+    """
+        A child class that overrides the error type for internal server errors.
+        This type typically relates to unexpected server errors.
+    """
+
     default_message = "Something went wrong on the server. Please try again later."
     status_code = 500
+
     def __init__(self, message: str = None, response: Optional[Any] = None, error_id: Optional[str] = None):
+        """
+            Initializes an InternalServerError object.
+
+            Args:
+                message (str): The error message. Defaults to None.
+                response (Optional[Any]): The response from the server. Defaults to None.
+                error_id (Optional[str]): The error ID. Defaults to None.
+        """
         super().__init__(message=message, response=response)
 
         self.error_id = error_id
 
     def to_dict(self):
+        
         data = super().to_dict()
         return self._add_additional_fields(data, ["error_id"])
     
 class ExceptionMapper:
+    """
+        A class used to map status codes to error classes.
+
+        This class provides methods to map a status code to an error class,
+        and to raise an error from a response.
+
+        Attributes:
+            _error_map (Dict[int, type]): A dictionary mapping status codes to error classes.
+    """
 
     _error_map = {
         400: BadRequestError,
@@ -110,7 +191,17 @@ class ExceptionMapper:
 
     @classmethod
     def from_response(cls, status_code: int, message: str = None, response: Any = None) -> ApiError:
-        
+        """
+            Creates an error object from a response.
+
+            Args:
+                status_code (int): The status code of the response.
+                message (str): The error message. Defaults to None.
+                response (Any): The response from the server. Defaults to None.
+
+            Returns:
+                ApiError: The error object.
+        """
         error_class = cls._error_map.get(status_code)
 
         if error_class:
@@ -121,19 +212,66 @@ class ExceptionMapper:
         
     @classmethod
     def raise_for_status(cls, status_code: int, message: str = None, response: Any = None):
+        """
+            Raises an error from a response.
 
+            Args:
+                status_code (int): The status code of the response.
+                message (str): The error message. Defaults to None.
+                response (Any): The response from the server. Defaults to None.
+
+            Raises:
+                ApiError: The error object.
+        """
         if status_code >= 400:
             raise cls.from_response(status_code, message, response)
         
     @classmethod
     def get_error_class(cls, status_code: int) -> type:
+        """
+            Gets the error class for a status code.
+
+            Args:
+                status_code (int): The status code of the response.
+
+            Returns:
+                type: The error class.
+        """
         return cls._error_map.get(status_code, ApiError)
         
 
 def handle_api_errors(default_return = None, log_errors = True):
+    """
+        A decorator that handles API errors and logs them if desired.
 
+        Args:
+            default_return (Any): The default return value when an error occurs. Defaults to None.
+            log_errors (bool): Determines whether errors are logged. Defaults to True.
+
+        Returns:
+            Callable: The decorated function.
+    """
     def decorator(func):
+        """
+            A decorator function that handles API errors and logs them if desired.
+
+            Args:
+                func (Callable): The function to be decorated.
+
+            Returns:
+                Callable: The decorated function.
+        """
         def wrapper(*args, **kwargs):
+            """
+                A wrapper function that handles API errors and logs them if desired.
+
+                Args:
+                    *args (Any): The positional arguments of the function.
+                    **kwargs (Any): The keyword arguments of the function.
+
+                Returns:
+                    Any: The result of the function if no error occurs, otherwise the default return value.
+            """
             try:
                 return func(*args, **kwargs)
             except ApiError as e:
