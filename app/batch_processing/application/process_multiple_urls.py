f3b84ca9f8f91058aded8a45a977ea5180804bdc
Angelou Lapad <ugwapings@gmail.com>
2025-12-11 13:57:29 +0800

    update

--- app/batch_processing/application/process_multiple_urls.py
+++ app/batch_processing/application/process_multiple_urls.py
@@ -4,8 +4,8 @@ from app.infrastructure.websocket.websocket_notifier import WebSocketNotifier
 from app.multilogin.application.multi_login_service import MultiLoginService
 from app.batch_processing.services.url_processor import URLProcessor
 from app.multilogin.application.profile_allocation_service import ProfileAllocationService
+from app.multilogin.domain.profile_lifecycle_manager import ProfileLifecycleManager
 from app.utils.concurrent_task_executor import ConcurrentTaskExecutor
-from app.multilogin.services.profile_lifecycle_manager import ProfileLifecycleManager
 from app.batch_processing.services.progress_notifier import BatchProgressNotifier
 from app.batch_processing.services.result_aggregator import BatchResultAggregator
 from app.batch_processing.services.url_processing_service import URLProcessingService
